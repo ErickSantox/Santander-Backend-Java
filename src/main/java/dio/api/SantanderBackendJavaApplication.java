1d85603458b9f0701d8fa66d3f519b5fdf4329fc
Erick Santos <118027969+ErickSantox@users.noreply.github.com>
2024-07-06 13:15:18 -0300

    Santander API

---
+++ src/main/java/dio/api/SantanderBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package dio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBackendJavaApplication.class, args);
+	}
+
+}
