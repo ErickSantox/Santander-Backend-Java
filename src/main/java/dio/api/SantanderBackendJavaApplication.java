b02879a150417a15bd17fdde2e5973e0bfe3dd7d
Erick Santos <118027969+ErickSantox@users.noreply.github.com>
2024-07-06 15:42:51 -0300

    ajustes

--- src/main/java/dio/api/SantanderBackendJavaApplication.java
+++ src/main/java/dio/api/SantanderBackendJavaApplication.java
@@ -1,8 +1,12 @@
 package dio.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
+@OpenAPIDefinition(servers = { @Server(url = "/",description = "default Server URL")})
 @SpringBootApplication
 public class SantanderBackendJavaApplication {
 
